8da2c143454c81f0098328396e454ee0d106f4ef
AlexandrTulai13@gmail.com <AlexandrTulai13@gmail.com>
2021-04-07 23:37:21 +0300

    Lecture 5. Task 1 - done.

---
+++ src/main/java/homework/lectures/lecture5/Product.java
@@ -0,0 +1,45 @@
+package homework.lectures.lecture5;
+
+public class Product {
+    private final String name;
+    private double regularPrice;
+    private double discount;
+    private double actualPrice;
+
+    public double getActualPrice() {
+        return actualPrice;
+    }
+
+    public void setActualPrice(double actualPrice) {
+        this.actualPrice = actualPrice;
+    }
+
+    public double getRegularPrice() {
+        return regularPrice;
+    }
+
+    public void setRegularPrice(double regularPrice) {
+        this.regularPrice = regularPrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", regularPrice=" + regularPrice +
+                ", discount=" + discount +
+                '}';
+    }
+}
