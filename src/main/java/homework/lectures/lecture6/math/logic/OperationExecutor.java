01588086bbecd5798b4f3734514807faaab2bb17
AlexandrTulai13@gmail.com <AlexandrTulai13@gmail.com>
2021-05-18 20:39:16 +0300

    Lecture 6-7. Task 2 - done. Draft_2

--- src/main/java/homework/lectures/lecture6/math/logic/OperationExecutor.java
+++ src/main/java/homework/lectures/lecture6/math/logic/OperationExecutor.java
@@ -1,11 +1,18 @@
 package homework.lectures.lecture6.math.logic;
 
-public class OperationExecutor {
+import java.util.List;
 
-    public void execute(MathOperation[] operations, Double a, Double b) {
+public class OperationExecutor<T> {
 
-        for (MathOperation numbers : operations) {
-            System.out.println(numbers.compute(a,b));
-        }
+    private final List<MathOperation<T>> operations;
+
+    public OperationExecutor (List<MathOperation<T>> operations){
+        this.operations =operations;
+    }
+
+    public void execute(T a,T b) {
+
+        operations.forEach(el -> System.out.println(el.compute(a,b)));
+
     }
 }
