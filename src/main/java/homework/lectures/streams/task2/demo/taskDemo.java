e4ba30f234df970b90c1f0c1566f8c8fc32256a0
AlexandrTulai13@gmail.com <AlexandrTulai13@gmail.com>
2021-06-21 17:47:16 +0300

    Streams - Task-3 - done.

--- src/main/java/homework/lectures/streams/task2/demo/taskDemo.java
+++ src/main/java/homework/lectures/streams/task2/demo/taskDemo.java
@@ -1,25 +1,22 @@
 package homework.lectures.streams.task2.demo;
 
 import homework.lectures.streams.task2.logic.ChangeUserStatus;
-import homework.lectures.streams.task2.model.User;
 import homework.lectures.streams.task2.repository.UserRepository;
 import org.apache.log4j.Logger;
-
-import java.util.List;
 
 public class taskDemo {
 
     private static final Logger log = Logger.getLogger(UserRepository.class);
 
     public static void main(String[] args) {
-        List<User> userList = UserRepository.getUserList();
+        var userList = UserRepository.getUserList();
         log.info(userList + "\n");
 
         ChangeUserStatus.updateUserStatus(userList);
         log.info("After user updates:");
         log.info(userList + "\n");
 
-        List<User> adultUsersList = ChangeUserStatus.userStatusChangeByAge(userList);
+        var adultUsersList = ChangeUserStatus.userStatusChangeByAge(userList);
         log.info("User list after user age updates:");
         log.info(userList + "\n");
         log.info("Adult user list:");
